4f6c180c4e02e75fee4fedd66f34aea48a7bc4e4
Macyl23 <ahmedzaidmacyl99@gmail.com>
2023-02-20 13:55:05 +0100

    Initial commit MongoDB configuration

--- API/config/db.ts
+++ API/config/db.ts
@@ -1,21 +1,16 @@
-import {getFirestore} from '@firebase/firestore'
-import {getStorage} from 'firebase/storage'
-import { initializeApp } from 'firebase/app';
-import {getAuth} from 'firebase/auth'
+// fonction permettant de se connecter à la base de donnée mongoose
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAcfbBOD0i8flYQmuIy0kIR3B9UdM2nXSE",
-  authDomain: "l3h1-f185d.firebaseapp.com",
-  projectId: "l3h1-f185d",
-  storageBucket: "l3h1-f185d.appspot.com",
-  messagingSenderId: "1001469802186",
-  appId: "1:1001469802186:web:9d596227512e58d385a633",
-  measurementId: "G-9DJ2GQJ1HT"
-};
+const mongoose=require('mongoose')
+const uri = "mongodb+srv://"+process.env.DB_USERPASS+"@cluster0.cxstilf.mongodb.net/?retryWrites=true&w=majority"
 
+mongoose.set('strictQuery', false);
+async function connect(){
+    try{
+        await mongoose.connect(uri)
+        console.log("Connected");
+    }catch(error:any){
+        console.error(error);
+    }
+}
 
-const app = initializeApp(firebaseConfig)
-const db = getFirestore(app)
-const auth = getAuth(app)
-const storage = getStorage(app)
-export = {db,auth, storage}
+export = {connect}
--- API/server.ts
+++ API/server.ts
@@ -1,8 +1,10 @@
 import express from 'express'
-import router from './routes/owner.routes'
-const routerEtb = require('./routes/etb.routes')
+// import router from './routes/owner.routes'
+// const routerEtb = require('./routes/etb.routes')
 import cors from 'cors'
 import bodyParser from 'body-parser'
+const dotenv = require("dotenv").config({path: "./config/.env"})
+const db = require ("./config/db")
 const port = 5000
 const app:express.Application = express()
 
@@ -11,9 +13,11 @@ const app:express.Application = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors())
-app.use("/api", router)
-app.use("/apiEtb", routerEtb)
+// app.use("/api", router)
+// app.use("/apiEtb", routerEtb)
 
+
+db.connect()
 app.get(("/"), (req:express.Request, res:express.Response) => {
     res.status(200).send("Running")
 })
