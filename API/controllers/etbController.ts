c0528e2cc141f02ef3168243855907a59b60defa
Macyl23 <ahmedzaidmacyl99@gmail.com>
2023-02-23 15:38:23 +0100

    Modification de l'affichage des établissements

--- API/controllers/etbController.ts
+++ API/controllers/etbController.ts
@@ -5,8 +5,8 @@ const ObjectId = require('mongoose').Types.ObjectId
 const getAllEtablissements =  async (req,res) => {
 
     try{
-        const user = await model.Proprio.findById(req.params.id)
-        res.status(200).send(user.etablissement)
+        const etbs = await model.Etb.find({userId: req.params.id})
+        res.status(200).send(etbs)
         
     }catch(error:any){
         res.status(400).send(error.message)
