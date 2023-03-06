11151463db6c6207955e96dc1f291c52bd5dd1d9
Macyl23 <ahmedzaidmacyl99@gmail.com>
2023-03-06 21:19:42 +0100

    Modification du formulaire d'ajout d'établissement

--- app_web/lib/views/Location_Reference/location_reference.dart
+++ app_web/lib/views/Location_Reference/location_reference.dart
@@ -1,12 +1,7 @@
 // ignore_for_file: avoid_print, unused_field
 
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-
-import '../../constants/app_colors.dart';
 
 class LocationReference extends StatefulWidget {
   const LocationReference({super.key});
@@ -16,35 +11,14 @@ class LocationReference extends StatefulWidget {
 }
 
 class _LocationReferenceState extends State<LocationReference> {
-  File? _pickedImage;
-  Uint8List webImage = Uint8List(8);
+  final List<XFile> _imageFiles = [];
+  final ImagePicker imagePicker = ImagePicker();
 
   Future<void> _pickImage() async {
-    if (!kIsWeb) {
-      final ImagePicker picker = ImagePicker();
-      XFile? image = await picker.pickImage(source: ImageSource.gallery);
-      if (image != null) {
-        File selected = File(image.path);
-        setState(() {
-          _pickedImage = selected;
-        });
-      } else {
-        print('no Image');
-      }
-    } else if (kIsWeb) {
-      final ImagePicker picker = ImagePicker();
-      XFile? image = await picker.pickImage(source: ImageSource.gallery);
-      if (image != null) {
-        Uint8List f = await image.readAsBytes();
-        setState(() {
-          webImage = f;
-          _pickedImage = File('a');
-        });
-      } else {
-        print('no Image');
-      }
-    } else {
-      print('Error');
+    final List<XFile> selectedImages = await imagePicker.pickMultiImage();
+    if (selectedImages.isNotEmpty) {
+      _imageFiles.addAll(selectedImages);
+      setState(() {});
     }
   }
 
@@ -53,88 +27,67 @@ class _LocationReferenceState extends State<LocationReference> {
     return Scaffold(
       body: Center(
         child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          SizedBox(
-            width: 1000,
-            child: Column(
-              children: 
-              [
-                Padding(padding: EdgeInsets.all(30)),
-              
-            TextFormField(
-              textInputAction: TextInputAction.next,
-              cursorColor: primaryColor,
-              decoration: const InputDecoration(
-                hintText: "Nom d'établissement",
-                prefixIcon: Padding(
-                  padding: EdgeInsets.all(defaultPadding),
-                  child: Icon(Icons.home),
-                ),
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(
+                width: 1000,
+                child: Column(children: const [
+                  Padding(
+                    padding: EdgeInsets.all(30),
+                  ),
+                  Text('Nom d\'etablissement'),
+                  TextField(),
+                  Padding(padding: EdgeInsets.only(top: 30)),
+                  Text('Adresse de l\'etablissement'),
+                  TextField(),
+                  Padding(padding: EdgeInsets.only(top: 30)),
+                  Text('Ajouter des images'),
+                  Padding(padding: EdgeInsets.only(top: 30)),
+                ])),
+            InkWell(
+              onTap: () {
+                _pickImage();
+              },
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(5.0),
+                child: Container(
+                    color: Colors.blue,
+                    padding: const EdgeInsets.all(10.0),
+                    child: const Icon(Icons.photo_camera)),
               ),
             ),
-
-            const Padding(padding: EdgeInsets.only(top: 30)),
-            TextFormField(
-              textInputAction: TextInputAction.next,
-              cursorColor: primaryColor,
-              decoration: const InputDecoration(
-                hintText: "Adresse d'établissement",
-                prefixIcon: Padding(
-                  padding: EdgeInsets.all(defaultPadding),
-                  child: Icon(Icons.location_on),
-                ),
-              ),
-            ),          
-            const Padding(padding: EdgeInsets.only(top: 30)),
-            TextFormField(
-              textInputAction: TextInputAction.next,
-              cursorColor: primaryColor,
-              decoration: const InputDecoration(
-                hintText: "Ajouter des images",
-                prefixIcon: Padding(
-                  padding: EdgeInsets.all(defaultPadding),
-                  child: Icon(Icons.image),
-                ),
-              ),
-            ),     
-            ElevatedButton(
-                onPressed: () {
-                  _pickImage();
-                },
-                child: const Icon(Icons.add)
+            Expanded(
+              child: Padding(
+                  padding: const EdgeInsets.all(8.9),
+                  child: GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 7),
+                      scrollDirection: Axis.vertical,
+                      padding: const EdgeInsets.only(right: 5.0),
+                      itemCount: _imageFiles.length,
+                      itemBuilder: (context, index) {
+                        return Image.network(
+                          _imageFiles[index].path,
+                          width: 20,
+                          height: 20,
+                        );
+                      })),
             ),
-
-            const Padding(padding: EdgeInsets.only(top: 30)),
-            ButtonBar(
-              layoutBehavior: ButtonBarLayoutBehavior.constrained,
-              children: [
-                InkWell(
-                  onTap: () {},
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(5.0),
-                    child: Container(
-                      color: Colors.blue,
-                      padding: const EdgeInsets.all(10.0),
-                      child: const Text(
-                        'Envoyer',
-                        textAlign: TextAlign.center,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(fontWeight: FontWeight.bold,
-                        color: Colors.white),
-                      )
-                    )
-                  )
-                )
-              ]
-            )
-            ]
-            )
-          )
-        ]
+            const Padding(padding: EdgeInsets.only(right: 30)),
+            Center(
+              child: MaterialButton(
+                  color: Colors.blue,
+                  onPressed: () {},
+                  child: const Text(
+                    'Submit',
+                    style: TextStyle(color: Colors.white),
+                  )),
+            ),
+            const Padding(padding: EdgeInsets.only(top: 30))
+          ],
         ),
       ),
     );
-                        
-}
+  }
 }
