0a06ab11ecdd7bbb9f70732c7f12441a4f35aaae
Macyl23 <ahmedzaidmacyl99@gmail.com>
2023-03-17 00:56:44 +0100

    Design affichage d'un établissement

--- app_mobile/lib/features/screens/Home/Widgets/Body/signle_etablissement.dart
+++ app_mobile/lib/features/screens/Home/Widgets/Body/signle_etablissement.dart
@@ -1,10 +1,8 @@
-
 import 'package:flutter/material.dart';
 import 'package:myevent/features/screens/Detail_page/detail_page.dart';
 
 import 'package:myevent/features/screens/Home/home_services.dart';
 
-import '../../../../../constants/utils.dart';
 import '../../../../../models/etablissement.dart';
 
 class SingleEtb extends StatefulWidget {
@@ -41,49 +39,83 @@ class _SingleEtbState extends State<SingleEtb> {
     double ffem = fem * 0.97;
     return etbs == null
         ? const Text("No establishments near to your lcoation")
-        : Column(
-            children: [
-                SizedBox(
-                    
-                    height: 300,
-                    child: ListView.builder(
-                      scrollDirection: Axis.vertical,
-                      padding: const EdgeInsets.only(top: 15),
-                      itemCount: etbs!.length,
-                      itemBuilder: (context, index) {
-                        final etb = etbs![index];
-                        return GestureDetector(
-                            onTap: () {
-                              Navigator.pushNamed(
-                                context,
-                                DetailPage.routeName,
-                                arguments: etb,
-                              );
-                            },
-                            child: Column(children: [
-                              SizedBox(
-                                height: 130,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(10),
+        : Column(children: [
+            SizedBox(
+                height: 300,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.vertical,
+                  itemCount: etbs!.length,
+                  itemBuilder: (context, index) {
+                    final etb = etbs![index];
+                    return GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(
+                            context,
+                            DetailPage.routeName,
+                            arguments: etb,
+                          );
+                        },
+                        child: Container(
+                            margin: const EdgeInsets.only(top: 10.0),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20.0)),
+                            child: Row(children: [
+                              Flexible(
+                                flex: 1,
+                                child: ClipRRect(
+                                  borderRadius: const BorderRadius.only(
+                                      topLeft: Radius.circular(20.0),
+                                      bottomLeft: Radius.circular(20.0)),
                                   child: Image.network(
                                     etb.images[0],
+                                    fit: BoxFit.cover,
                                   ),
                                 ),
                               ),
-                              Text(
-                                etb.nameEtb,
-                                style: SafeGoogleFont(
-                                  'Plus Jakarta Sans',
-                                  fontSize: 16 * ffem,
-                                  fontWeight: FontWeight.w700,
-                                  height: 1.5 * ffem / fem,
-                                  letterSpacing: 0.08 * fem,
-                                  color: Color(0xff111111),
+                              Flexible(
+                                flex: 2,
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 10.0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        etb.nameEtb,
+                                        textAlign: TextAlign.left,
+                                        maxLines: 2,
+                                        style: const TextStyle(
+                                          fontSize: 18,
+                                        ),
+                                      ),
+                                      const SizedBox(height: 5),
+                                      Row(
+                                        children: [
+                                          const Icon(Icons.location_on),
+                                          const SizedBox(height: 5),
+                                          Text(
+                                            etb.lieu,
+                                            style:
+                                                const TextStyle(fontSize: 10),
+                                            maxLines: 3,
+                                          )
+                                        ],
+                                      ),
+                                      const SizedBox(height: 5),
+                                      Text("${etb.prix}€",
+                                          style: const TextStyle(
+                                              fontSize: 20.0,
+                                              fontWeight: FontWeight.bold))
+                                    ],
+                                  ),
                                 ),
-                              ),
-                            ]));
-                      },
-                    ))
-              ]);
+                              )
+                            ])));
+                  },
+                ))
+          ]);
   }
 }
