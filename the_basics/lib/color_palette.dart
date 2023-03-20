2ba6d9739ee4fa96063c179495879302629d8b3d
Macyl23 <ahmedzaidmacyl99@gmail.com>
2023-03-21 00:09:04 +0100

    Suppression de the_basics de develop

--- the_basics/lib/color_palette.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ColorPalette {
-  final Color scaffoldBg = Color(0xFF0D0F14);
-  final Color searchBarFill = Color(0xFF141921);
-  final Color lcoationSelected = Color(0xFFD17741);
-  final Color locationUnselected = Color(0xFF525559);
-  final Color gradientTopLeft = Color(0xFF262B34);
-}
