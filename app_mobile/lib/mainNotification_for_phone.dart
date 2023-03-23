4882845db685b77522ed4d6185422a1c72b49002
Nguyen Huu Truc <nguyen.huutruc2001@gmail.com>
2023-03-23 17:29:42 +0100

    changement d'un file

--- app_mobile/lib/mainNotification_for_phone.dart
+++ app_mobile/lib/mainNotification_for_phone.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:flutter/material.dart';
 
-/*void main() {
+void main() {
   AwesomeNotifications().initialize(
       null,
       [
@@ -73,4 +73,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-}*/
+}
